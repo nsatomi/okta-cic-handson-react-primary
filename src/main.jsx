f2dd5b5e0150b10503323b1a7c74a4375c198121
nsatomi <naoyasatomi22@gmail.com>
2024-06-11 15:43:28 +0900

    Auth0Provider 登録

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Home from './routes/Home.jsx';
 import Profile from './routes/Profile.jsx';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const router = createBrowserRouter([
   {
@@ -18,6 +19,14 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Auth0Provider
+      domain='https://gray-mule-75111.cic-demo-platform.auth0app.com'
+      clientId='mdAoAoHT9Hjo0HGq2MkbIeIKZazvog1B'
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <RouterProvider router={router} />
+    </Auth0Provider>
   </React.StrictMode>,
 )
